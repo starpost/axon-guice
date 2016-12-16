9eef4869e829e5f88bf274b39ee0f32fe6f6324c
唐家文 <km.tong@starpost.cn>
2016-12-16 10:20:30 +0800

    null lock annotation

--- src/main/java/com/google/code/axonguice/repository/eventsourcing/NullLock.java
+++ src/main/java/com/google/code/axonguice/repository/eventsourcing/NullLock.java
@@ -1,6 +1,8 @@
 package com.google.code.axonguice.repository.eventsourcing;
 
-import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
@@ -10,7 +12,7 @@ import com.google.inject.BindingAnnotation;
 
 @BindingAnnotation
 @Retention(RUNTIME)
-@Target(TYPE)
+@Target({ FIELD, PARAMETER, METHOD })
 public @interface NullLock {
 
 }
